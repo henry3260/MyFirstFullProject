eda7979cee5d7d0c7e3ba14ee4a638e401ccbd28
Henry <henryhenry0512@gmail.com>
2024-04-28 03:23:23 +0800

    Initial commit

---
+++ my-app/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <main>
+      <h1>$400<span>.00</span></h1>
+    </main>
+  );
+}
+
+export default App;
---
+++ src/App.js
@@ -0,0 +1,57 @@
+import './App.css';
+
+function App() {
+  return (
+    <main>
+      <h1>$400<span>.00</span></h1>
+      <form>
+        <div className="basic">
+            <input type="text" placeholder={'+200 new samsung TV'} />
+            <input type="datetime-local" />
+        </div>
+        <div className="description">
+          <input type="text" placeholder={'description'}></input>
+        </div>
+        <button type="submit">Add new transaction</button>
+      </form>
+      <div className="transactions">
+        <div className="transaction">
+          <div className="left">
+            <div className="name">New samsung TV</div>
+            <div className="description">it was time for new tv</div>
+          </div>
+          <div className="right">
+            <div className="price">$500</div>
+            <div className="datetime">2022-12-18 15:45</div>
+          </div>
+        </div>
+      </div>
+      <div className="transactions">
+        <div className="transaction">
+          <div className="left">
+            <div className="name">New samsung TV</div>
+            <div className="description">it was time for new tv</div>
+          </div>
+          <div className="right">
+            <div className="price">$500</div>
+            <div className="datetime">2022-12-18 15:45</div>
+          </div>
+        </div>
+      </div>
+      <div className="transactions">
+        <div className="transaction">
+          <div className="left">
+            <div className="name">New samsung TV</div>
+            <div className="description">it was time for new tv</div>
+          </div>
+          <div className="right">
+            <div className="price">$500</div>
+            <div className="datetime">2022-12-18 15:45</div>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+}
+
+export default App;
